59437e392296a250dbc927a34a0acadf464789b8
augustosalazar <augustosalazar@uninorte.edu.co>
2015-05-06 15:26:30 -0500

    Add task to service

--- app/src/main/java/com/augustosalazar/simpleservice/MyService.java
+++ app/src/main/java/com/augustosalazar/simpleservice/MyService.java
@@ -3,9 +3,39 @@ package com.augustosalazar.simpleservice;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class MyService extends Service {
+
+    private static final String TAG = "MyService";
+    private Timer mTimer = new Timer();
+    private static boolean isRunning = false;
+    private int counter = 0;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.i(TAG, "Service Started.");
+        mTimer.scheduleAtFixedRate(new MyTask(), 0, 100L);
+        isRunning = true;
+    }
+
+    private class MyTask extends TimerTask {
+        @Override
+        public void run() {
+            Log.i(TAG, "Timer doing work." + counter);
+            try {
+                counter += 1;
+            } catch (Throwable t) {
+                Log.e("TimerTick", "Timer Tick Failed.", t);
+            }
+        }
+    }
+
     @Override
     public IBinder onBind(Intent intent) {
         return null;
